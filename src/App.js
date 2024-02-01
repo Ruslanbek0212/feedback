0a9b7429ef8c5cdbab3f314cf7e76c7aa5386e8b
ruslanbek <sunnatov.ruslanbek@bk.ru>
2024-02-01 21:29:03 +0500

    start project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-[100px] text-[#4b27af] '>feedback</h1>
+      
+    </div>
+  )
+}
+
+export default App
